af001ad703c8d60654f5f67201a69b7b00f99762
ademhatay <66277966+ademhatay@users.noreply.github.com>
2025-03-17 12:09:23 +0300

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-ssh root@128.199.25.122 <<EOF
-  cd drfblogproject
-  git pull
-  source /opt/envs/drfblogproject/bin/activate
-  pip install -r requirements.txt
-  ./manage.py makemigrations
-  ./manage.py migrate  --run-syncdb
-  sudo service gunicorn restart
-  sudo service nginx restart
-  exit
-EOF
+cd django-blog
+git pull
+source env/bin/activate
+pip install -r requirements.txt
+./manage.py makemigrations
+./manage.py migrate --run-syncdb
+sudo service gunicorn restart
+sudo service nginx restart
